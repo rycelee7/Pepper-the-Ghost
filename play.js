8dd65168d5e36470be1fcdbdc6677092e584c9e3
Royce Lee <roycelee@Royces-MacBook-Pro.local>
2021-04-28 18:05:49 -0400

    work

---
+++ play.js
@@ -0,0 +1,121 @@
+let pepperIdle = false;
+let pepperWaves = false;
+let pepperSquare = false;
+
+// variable to allow me to check the amount of frames
+let x = 0;
+
+let pepperWaveFrame;
+let pepperSquareFrame;
+
+let client = null;
+
+try {
+  client = mqtt.connect("wss://test.mosquitto.org:8081");
+} catch (e) {
+  console.log(e);
+}
+
+if (client) {
+  // when connected
+  client.on("connect", function () {
+    console.log("subscribing...");
+    // subscribe to this topic
+    client.subscribe("pepper");
+  });
+
+  // when message receieved
+  client.on("message", function (topic, message) {
+    const msgString = message.toString();
+
+    console.log(msgString);
+
+    if (msgString.includes("wave")) {
+      pepperWaves = true;
+      pepperIdle = false;
+      console.log("successful wave!");
+    }
+
+    if (msgString.includes("counter-clockwise")) {
+      pepperSquare = true;
+      pepperIdle = false;
+      console.log("successful square!");
+    }
+    //   client.end();
+  });
+}
+
+// function preload() {}
+
+function setup() {
+  pepperIdleGif = loadImage("animations/pepper.gif");
+  pepperWaveGif = loadImage("animations/waving.gif");
+  pepperSquareGif = loadImage("animations/square.gif");
+
+  var canvas = createCanvas(450, 800);
+  // Move the canvas so it’s inside our <div id="sketch-holder">.
+  canvas.parent("sketch-holder");
+}
+
+function draw() {
+  background(0);
+
+  // gets the max frames of Idle Gif and Waving Gif
+  let maxIdle = pepperIdleGif.numFrames() - 1;
+  let maxWave = pepperWaveGif.numFrames() - 1;
+  let maxSquare = pepperSquareGif.numFrames() - 1;
+
+  // lets me know how many frames there are
+  if (x == 0) {
+    console.log(maxIdle);
+    console.log(maxWave);
+    console.log(maxSquare);
+    x++;
+  }
+
+  if (pepperIdle) {
+    image(pepperIdleGif, 0, 0, 375, 750);
+    pepperIdleGif.play();
+  } else {
+    pepperIdleGif.reset();
+  }
+
+  pepperWaveFrame = pepperWaveGif.getCurrentFrame();
+  pepperSquareFrame = pepperSquareGif.getCurrentFrame();
+  // text(pepperWaveFrame, 400, 90);
+  // fill("white");
+
+  if (pepperWaves) {
+    // console.log("Triggering Pepper Waves");
+    image(pepperWaveGif, 0, 0, 375, 750);
+    pepperWaveGif.play();
+    if (pepperWaveFrame == 93) {
+      pepperWaves = false;
+      pepperIdle = true;
+    }
+  } else {
+    pepperWaveGif.reset();
+  }
+
+  if (pepperSquare) {
+    // console.log("Triggering Pepper Waves");
+    image(pepperSquareGif, 0, 0, 375, 750);
+    pepperSquareGif.play();
+    if (pepperSquareFrame == 144) {
+      pepperSquare = false;
+      pepperIdle = true;
+    }
+  } else {
+    pepperSquareGif.reset();
+  }
+}
+
+function mousePressed() {
+  if (pepperSquare) {
+    pepperSquare = false;
+    pepperIdle = true;
+  } else {
+    pepperSquare = true;
+    pepperIdle = false;
+  }
+}
---
+++ work.js
@@ -0,0 +1,134 @@
+let countdown = 0; // variable to set/clear intervals
+let seconds = 1200; // seconds left on the clock
+let workTime = 20;
+let breakTime = 5;
+let isBreak = true;
+let isPaused = true;
+
+const status = document.querySelector("#status");
+const timerDisplay = document.querySelector(".timerDisplay");
+const startBtn = document.querySelector("#start-btn");
+const resetBtn = document.querySelector("#reset");
+const workMin = document.querySelector("#work-min");
+const breakMin = document.querySelector("#break-min");
+
+const alarm = document.createElement("audio");
+
+// A bell sound will play when the timer reaches 0
+alarm.setAttribute(
+  "src",
+  "https://www.soundjay.com/misc/sounds/bell-ringing-05.mp3"
+);
+
+// Adding event listeners for the start and reset button
+startBtn.addEventListener("click", () => {
+  clearInterval(countdown);
+  isPaused = !isPaused;
+
+  // if it is not paused, set countdown timer to decrease every 1000 ms
+  if (!isPaused) {
+    countdown = setInterval(timer, 1000);
+  }
+});
+
+// reset button checker, if it is clicked, set countdown to 0 and pause
+resetBtn.addEventListener("click", () => {
+  clearInterval(countdown);
+  seconds = workTime * 60;
+  countdown = 0;
+  isPaused = true;
+  isBreak = true;
+});
+
+// timer, which will handle the countdown
+function timer() {
+  seconds--;
+
+  // if seconds is less than 0, then clear the countdown, play the alarm,
+  if (seconds < 0) {
+    clearInterval(countdown);
+    alarm.currentTime = 0;
+    alarm.play();
+
+    // checking if it is break, if break, then breaktime; if not, worktime --> set that * 60 to seconds
+    seconds = (isBreak ? breakTime : workTime) * 60;
+    isBreak = !isBreak;
+  }
+}
+
+// update work times and break times
+let increment = 1;
+
+let incrementFunctions = {
+  "#work-plus": function () {
+    workTime = Math.min(workTime + increment, 60);
+  },
+  "#work-minus": function () {
+    workTime = Math.max(workTime - increment, 1);
+  },
+  "#break-plus": function () {
+    breakTime = Math.min(breakTime + increment, 60);
+  },
+  "#break-minus": function () {
+    breakTime = Math.max(breakTime - increment, 5);
+  },
+};
+
+for (var key in incrementFunctions) {
+  if (incrementFunctions.hasOwnProperty(key)) {
+    document.querySelector(key).onclick = incrementFunctions[key];
+  }
+}
+
+// Update html content
+function countdownDisplay() {
+  let minutes = Math.floor(seconds / 60);
+  let remainderSeconds = seconds % 60;
+  timerDisplay.textContent = `${minutes}:${
+    remainderSeconds < 10 ? "0" : ""
+  }${remainderSeconds}`;
+}
+
+function buttonDisplay() {
+  if (isPaused && countdown === 0) {
+    startBtn.textContent = "START";
+  } else if (isPaused && countdown !== 0) {
+    startBtn.textContent = "Continue";
+  } else {
+    startBtn.textContent = "Pause";
+  }
+}
+
+function updateHTML() {
+  countdownDisplay();
+  buttonDisplay();
+  isBreak
+    ? (status.textContent = "Keep Working")
+    : (status.textContent = "Take a Break!");
+  workMin.textContent = workTime;
+  breakMin.textContent = breakTime;
+}
+
+var y = document.getElementsByClassName("reading");
+y[0].style.display = "none";
+
+function startPomodoro() {
+  var x = document.getElementsByClassName("hideDivs");
+  if (x[0].style.display === "none") {
+    x[0].style.display = "block";
+    y[0].style.display = "none";
+  } else {
+    x[0].style.display = "none";
+    y[0].style.display = "block";
+  }
+}
+
+function setup() {
+  var canvas = createCanvas(450, 800);
+  // Move the canvas so it’s inside our <div id="sketch-holder">.
+  canvas.parent("work-sketch-holder");
+}
+
+window.setInterval(updateHTML, 100);
+
+document.onclick = updateHTML;
